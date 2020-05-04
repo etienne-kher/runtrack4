32f2719ae2cdfe5aabfaf9fa548415e6fced8273
Kher <etienne.kher@laplateforme.io>
2020-05-04 09:18:14 +0200

    job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>job01</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+Créez un fichier index.php contenant les balises html de base (doctype,
+html, head, body). Créez un fichier style.css et ajouter des propriétés
+permettant à votre body d’avoir une couleur de background #88c1d0.
+A l’aide des media queries, cette couleur doit évoluer de la façon suivante :
+
+- #ffca4b lorsque la résolution est comprise entre 1680 et 1920
+- #275fa0 lorsque la résolution est comprise entre 1280 et 1680
+- #ffffff lorsque la résolution est inférieure à 1280
+</body>
+</html>
