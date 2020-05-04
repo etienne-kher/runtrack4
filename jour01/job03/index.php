3dbf2bfaa51a67595eaa3b79abf0d1e2740c45f6
Kher <etienne.kher@laplateforme.io>
2020-05-04 12:43:12 +0200

    job03 et job04

---
+++ jour01/job03/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>job03</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<section class="section">ga</section>
+	<section class="section">bu</section>
+	<section class="section">zo</section>
+	<section class="section">meu</section>
+</body>
+</html>
---
+++ jour01/job04/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>job04</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<header class="menu">
+			<input class="burger" type="checkbox">
+			<nav>
+				<a href="">Accueil</a>
+				<a href="">Présentation</a>
+				<a href="">Contact</a>
+			</nav>
+	</header>
+</body>
+</html>
+
+<!--
+Reprenez vos fichiers index.php et style.css. Ajoutez en haut de la page,
+un header qui contient une liste de trois liens : Accueil, Présentation,
+Contact. Lorsque la résolution est inférieure ou égale à 767. Les liens
+disparaissent pour laisser place à un hamburger button. Lorsque l’on clique
+sur celui-ci, les liens apparaissent. Si on re-clique, les liens disparaissent.
+!-->
