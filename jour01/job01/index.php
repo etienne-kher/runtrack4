c5115d046979452104457d8094cde35271c690da
Kher <etienne.kher@laplateforme.io>
2020-05-04 09:03:31 +0200

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>job01</title>
+	<link rel="stylesheet" type="text/css" media="screen and (min-width:1680px) and (max-width:1920px)" href="style1.css">
+	<link rel="stylesheet" type="text/css" media="screen and (min-width:1280px )and (max-width:1680px)" href="style2.css" >
+	<link rel="stylesheet" type="text/css" media="screen and (min-width:1920px)" href="style3.css"><!--provisoir tant que sujet foiré!-->
+	<link rel="stylesheet" type="text/css" media="screen and (max-width:1280px)" href="style4.css" >
+</head>
+<body>
+Créez un fichier index.php contenant les balises html de base (doctype,
+html, head, body). Créez quatre fichiers de style :
+- style1.css : modifie la couleur du background en #88c1d0.
+- style2.css : modifie la couleur du background en #ffca4b.
+- style3.css : modifie la couleur du background en #275fa0.
+- style4.css : modifie la couleur du background en #ffffff.
+Dans votre code html, linkez la bonne feuille de style en fonction de la
+largeur de l’écran de l’utilisateur : style1.css lorsque la résolution est
+comprise entre 1680px et 1920px, style2.css entre 1280px et 1680px et
+style4.css lorsqu’elle est inférieure à 1280.
+HTML suffit, vous pouvez oublier php.
+</body>
+</html>
