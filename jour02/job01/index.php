5273e8a58b1bb860bbcb004ac8ab9c92f43c3bd2
Kher <etienne.kher@laplateforme.io>
2020-05-05 11:50:11 +0200

    job1 2 3 4

---
+++ jour02/job01/index.php
@@ -0,0 +1,37 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+     
+
+    </head>
+
+    <body class="">
+     	<header class="">
+     		<nav class="">
+     			<a href="index.php" class="">Accueil</a> <a  class=""href="index.php">Inscription</a> <a class="" href="index.php">Connexion</a> <a  class="" href="index.php">Rechercher</a>
+     		</nav>
+     	</header>
+     	<section class="">
+     		<form class="">
+     			<input class="" type="text" name="prenom">
+     			<input class="" type="text" name="nom">
+     			<input class="" type="text" name="adresse">
+     			<input class="" type="password" name="mdp">
+     			<input class="" type="password" name="remdp">
+     			<label class="">passions:</label>
+     			<label class="">informatique</label>
+     			<input class="" type="checkbox" name="informatique">
+     			<label class="">voyage</label>
+     			<input class="" type="checkbox" name="voyages">
+     			<label class="">sport</label>
+     			<input class="" type="checkbox" name="sport">
+     			<label class="">lecture</label>
+     			<input class="" type="checkbox" name="lecture">
+				<input class="" type="submit" name="env">
+     		</form>
+     	</section>
+		<footer class="">
+			<a href="index.php" class="">Accueil</a> <a  class="" href="index.php">Inscription</a> <a class="" href="index.php">Connexion</a> <a  class="" href="index.php">Rechercher</a>
+		</footer>
+    </body>
+  </html>
---
+++ jour02/job04/index.php
@@ -0,0 +1,36 @@
+ <!DOCTYPE html>
+  <html>
+    <head>
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    </head>
+      <header class="blue ">
+        <nav class="container cyan row">
+          <a href="index.php" class="col s3 center">Accueil</a> <a  class="col s3 center"href="index.php">Inscription</a> <a class="col s3 center" href="index.php">Connexion</a> <a  class="col s3 center" href="index.php">Rechercher</a>
+        </nav>
+      </header>
+      <section class="">
+        <form class="">
+          <input class="" type="text" name="prenom">
+          <input class="" type="text" name="nom">
+          <input class="" type="text" name="adresse">
+          <input class="" type="password" name="mdp">
+          <input class="" type="password" name="remdp">
+          <label class="">passions:</label>
+          <label class="">informatique</label>
+          <input class="" type="checkbox" name="informatique">
+          <label class="">voyage</label>
+          <input class="" type="checkbox" name="voyages">
+          <label class="">sport</label>
+          <input class="" type="checkbox" name="sport">
+          <label class="">lecture</label>
+          <input class="" type="checkbox" name="lecture">
+        <input class="" type="submit" name="env">
+        </form>
+      </section>
+    <footer class="row yellow">
+      <a href="index.php" class="col s3 center">Accueil</a> <a  class="col s3 center" href="index.php">Inscription</a> <a class="col s3 center" href="index.php">Connexion</a> <a  class="col s3 center" href="index.php">Rechercher</a>
+    </footer>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+  </html>
+
